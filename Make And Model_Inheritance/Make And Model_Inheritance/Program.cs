818a611824e89e5a6b9704112123d273189c7e03
Abdullah Alsalem <abdullahsalem18@gmail.com>
2021-05-03 02:50:50 +0300

    Done
    Done

---
+++ Make And Model_Inheritance/Make And Model_Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Make_And_Model_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string brandName = Console.ReadLine();
+            string modelName = Console.ReadLine();
+
+            Car car = new Car();
+            car.Brand = brandName;
+            car.Model = modelName;
+
+            car.ShowBrand();
+            car.ShowModel();
+        }
+        class Vehicle
+        {
+            public string Brand { get; set; }
+
+            public void ShowBrand()
+            {
+                Console.WriteLine("Brand: " + Brand);
+            }
+        }
+
+        //complete the Car class
+        class Car : Vehicle
+        {
+            public string Model { get; set; }
+            
+            public void ShowModel()
+            {
+                Console.WriteLine("Model: " + Model);
+            }
+        }
+    }
+}
